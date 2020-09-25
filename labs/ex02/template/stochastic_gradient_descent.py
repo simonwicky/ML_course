1cca361de36671007eccfd9edfd710a52fbbdea9
Simon Wicky <simon.wicky@epfl.ch>
2020-09-25 10:24:19 +0200

    lab2 ex6

--- labs/ex02/template/stochastic_gradient_descent.py
+++ labs/ex02/template/stochastic_gradient_descent.py
@@ -3,18 +3,27 @@
 
 def compute_stoch_gradient(y, tx, w):
     """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient computation.It's same as the gradient descent.
-    # ***************************************************
-    raise NotImplementedError
+    e = y[:,np.newaxis] - tx @ w[:,np.newaxis]
+    return - (tx.T[:,np.newaxis] @ e) / tx.shape[0]
 
 
-def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
+def stochastic_gradient_descent(
+        y, tx, initial_w, batch_size, max_iters, gamma):
     """Stochastic gradient descent algorithm."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: implement stochastic gradient descent.
-    # ***************************************************
-    raise NotImplementedError
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        
+        loss = compute_loss(y,tx,w)
+        for minibatch_y, minibatch_tx in batch_iter(y,tx, batch_size):
+            gradient = compute_stoch_gradient(minibatch_y,minibatch_tx,w).flatten()
+        w = w - gamma * gradient
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
     return losses, ws
